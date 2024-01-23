c2aa26d0fd7fa8afdbf65e27b6f9eb91df2b04eb
JESSICA SAMPAIO PINTO FRANCISCO <jessica.spfrancisco@senacsp.edu.br>
2024-01-23 20:34:01 -0300

    Atualização

--- Biblioteca/script.js
+++ Biblioteca/script.js
@@ -4,7 +4,36 @@ class MaterialBibliografico {
         this.autor = autor;
         this.disponivel = true;
     }
+
+    realizarEmprestimo() {
+        if (this.disponivel == true) {
+            this.disponivel = false;
+            return true; // Emprestimo realizado com sucesso
+        }else {
+            return false; //Material já emprestado
+        }
+    }
+    
+    realizarDevolucao(){
+     if (this.disponivel == false){
+        this.disponivel = true;
+        return true; // Devolução realizada com sucesso
+     }else{
+        return false; // Material já empretado
+     }
+    }
+
+    //     realizarDevolucao2() {
+    //     if(this.disponivel) {
+    //         return false; //Material já devolvido prevaimente
+    //     } else {
+    //         this.disponivel = true;
+    //         return true; //Devolução realizada com sucesso
+    //     }
+    // }
+    
 }
+
 
 class Livro extends MaterialBibliografico {
     constructor(titulo, autor, genero){
