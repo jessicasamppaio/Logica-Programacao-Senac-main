60b8d7757f2b517daf4b808166368852bb98459c
JESSICA SAMPAIO PINTO FRANCISCO <jessica.spfrancisco@senacsp.edu.br>
2024-02-05 22:07:34 -0300

    Atualizar os metodos das contas

--- aulas/aula09/contas-bancarias.js
+++ aulas/aula09/contas-bancarias.js
@@ -1,51 +1,104 @@
 class Cliente {
-     constructor(nome, cpf){
+    constructor(nome, cpf) {
         this.nome = nome;
         this.cpf = cpf;
-     }
+    }
 }
 
 class Conta {
-    constructor(cliente, numero, saldo){
+    constructor(cliente, numero, saldo) {
         this.cliente = cliente;
         this.numero = numero;
         this.saldo = saldo;
     }
-       sacar(valorParaSaque){
-        return true;
-       }
 
-       depositar(valorParaDeposito){
-        return true
-       }
+    sacar(valorSaque) {
+        if (this.saldo >= valorSaque && this.saldo > 0) {
+        // this.saldo = this.saldo - valorSaque;
+             this.saldo -= valorSaque;
+            return true;
 
-       transferir(valorTransferencia, cliente){
-        return true;
+        } else {
+            return false;
+        }
+    }
 
-       }
+
+
+    depositar(valorDeposito) {
+        if (valorDeposito > 0) {
+        // this.saldo = this.saldo + valorDeposito;
+            this.saldo -= valorSaque;
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    transferir(valorTransferencia, conta){
+    // if(this.saldo >= valorTransferencia && valorTransferencia > 0){
+        //     this.saldo -= valorTransferencia;
+        //     conta.saldo += valorTransferencia 
+       
+        if(this.sacar(valorTransferencia)){
+            conta.depositar(valorTransferencia) 
+            return true;
+        } else{
+            return false;
+        }
+       
+    }
 }
 
 class ContaCorrente extends Conta {
-    constructor(cliente, numero, saldo, limiteChequeEspeial){
+    constructor(cliente, numero, saldo, limiteChequeEspecial) {
         super(cliente, numero, saldo);
-        this.limiteChequeEspeial = limiteChequeEspeial;
+        this.limiteChequeEspecial = limiteChequeEspecial;
     }
+    sacar(valorSaque) {
+    //                {
+    //     if(this.saldo + this.limiteChequeEspecial >= valorSaque && valorSaque > 0){
+    //         this.saldo -= valorSaque; 
+    //     }
+    // }
 
-    sacar(valorSaque){
-      super(valorSaque);
-      return true;
+    const valorLimiteEspecial = this.saldo + this.limiteChequeEspecial;
+    if (valorSaque >= valorLimiteEspecial) {
+        return super.sacar(valorSaque);
+    }else{
+        return false
     }
-
+}
 }
 
 class ContaPoupanca extends Conta {
-    constructor(cliente, numero, saldo, taxaRendimento){
+    constructor(cliente, numero, saldo, taxaRendimento) {
         super(cliente, numero, saldo);
         this.taxaRendimento = taxaRendimento;
-    } 
+    }
 
-    aplicarRendimento(){
-
+    aplicarRendimento() {
+        this.saldo += this.saldo * this.taxaRendimento;
     }
 }
+
+let contas = [];
+let clientes = [];
+
+// let clienteA = new Cliente("Mizuki", "1234567890");
+// clientes.push(clienteA);
+// let clienteB = new Cliente("Aiko", "0987654321");
+// clientes.push(clienteB);
+
+// let contaX = new ContaCorrente(clienteA, 123, 100, 150);
+// contas.push(contaX);
+// let contaY = new ContaPoupanca(clienteB, 111, 100, 0.01);
+// contas.push(contaY);
+// let contaZ = new ContaCorrente(clienteB, 235, 0, 180);
+// contas.push(contaZ);
+
+// contaY.transferir(50, contaX)
+// console.log("Conta Y: ", contaY);
+// console.log("Conta X: ", contaX)
+
 
