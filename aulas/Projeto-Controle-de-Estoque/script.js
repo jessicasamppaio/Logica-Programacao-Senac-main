e8a664637445d87ade60ce6e12a32715ee32b967
Jéssica <jessicaspf@gmail.com>
2024-02-09 12:36:26 -0300

    Resolução atividade controle de estoque

--- aulas/Projeto-Controle-de-Estoque/script.js
+++ aulas/Projeto-Controle-de-Estoque/script.js
@@ -6,8 +6,7 @@ class Produto {
     }
 
     calcularValorTotal() {
-        let valorTotal = this.quantidadeDisponivel * this.precoUnitario;
-        return valorTotal;
+        return this.quantidadeDisponivel * this.precoUnitario;
     }
 }
 
@@ -19,9 +18,9 @@ class ProdutoPerecivel extends Produto {
 
     verificarValidade() {
         const dataAtual = new Date();
-        if (dataAtual <= this.dataValidade) {
-            console.log('O produto está no prazo de validade.')
-            return true
+        if (dataAtual < this.dataValidade) {
+            console.log('O produto está no prazo de validade.');
+            return true;
         } else {
             console.log('O produto está fora do prazo de validade.');
             return false;
@@ -42,31 +41,39 @@ class Estoque {
         for (let i = 0; i < this.listaProdutos.length; i++){
           if(this.listaProdutos[i].nome === nome){
             this.listaProdutos.splice(i, 1);
+            console.log(`Produto '${nome}' removido do estoque.`);
             break;
           }
         }
     }
 
     verificarEstoqueDisponivel(nome) {
-      
+        for (const produto of this.listaProdutos){
+            if(produto.nome === nome){
+                console.log(`Quantidade disponível de '${nome}': ${produto.quantidadeDisponivel}`);
+                return;
+            }
+        }
+        console.log(`Produto '${nome}' não encontrado no estoque.`);
     }
 
     calcularValorTotalEstoque() {
         let valorTotalEstoque = 0;
         for (const produto of this.listaProdutos){
-             valorTotalEstoque = valorTotalEstoque + produto.calcularValorTotal();
+             valorTotalEstoque += produto.calcularValorTotal();
         }
-        return valorTotalEstoque
-        }
+        console.log(`Valor total do estoque: R$${valorTotalEstoque.toFixed(2)}`);
     }
+}
+
 
 
 const meuEstoque = new Estoque();
 
-meuEstoque.adicionarProduto(new Produto("Macarrao", 100, 5));
-meuEstoque.verificarEstoqueDisponivel(new Produto("Leite", 50, 4));
+meuEstoque.adicionarProduto(new Produto("Macarrão", 100, 5));
+meuEstoque.adicionarProduto(new ProdutoPerecivel("Leite", 50, 4, new Date(2024, 1, 1)));
 
-meuEstoque.calcularValorTotalEstoque()
+meuEstoque.verificarEstoqueDisponivel("Macarrão");
+meuEstoque.verificarEstoqueDisponivel("Leite");
 
-
-
+meuEstoque.calcularValorTotalEstoque();
